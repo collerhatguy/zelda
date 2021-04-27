8983fdefdf9187e2bd63415d75575f89d3a6312c
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-27 13:00:05 -0400

    connected all the pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, {useEffect} from "react";
 import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import './style/App.css';
 import ItemsPage from "./components/ItemsPage";
+import PlacesPage from "./components/PlacesPage";
+import DungeonsPage from "./components/DungeonsPage";
+import BossesPage from "./components/BossesPage";
+import MonstersPage from "./components/MonstersPage";
+import CharactersPage from "./components/CharactersPage";
+import GamesPage from "./components/GamesPage";
 
 function App() {
   
@@ -18,6 +24,12 @@ function App() {
         </header>
         <Switch>
           <Route path={`/items`} component={ItemsPage} />
+          <Route path={`/places`} component={PlacesPage} />
+          <Route path={`/dungeons`} component={DungeonsPage} />
+          <Route path={`/bosses`} component={BossesPage} />
+          <Route path={`/monsters`} component={MonstersPage} />
+          <Route path={`/characters`} component={CharactersPage} />
+          <Route path={`/games`} component={GamesPage} />
         </Switch>
       </Router>
     </div>
