d23c2926969c7818b2e300f79d073198c67944b0
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-27 12:20:39 -0400

    api hook complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-
+import React, {useEffect} from "react";
+import useAPI from "./hooks/useAPI";
 import './App.css';
 
 function App() {
+  const data = useAPI(0);
+  useEffect(() => {
+    console.log(data);
+  }, [data])
   return (
     <div className="App">
     </div>
--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,19 +1,28 @@
-import {} from "react";
+import {useState, useEffect} from "react";
 
-export default function useAPI() {
+export default function useAPI(number) {
     const queries = ["items", "places", "dungeons", "bosses", "monsters", "characters", "games"];
-    fetch("https://the-legend-of-zelda.p.rapidapi.com/bosses", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": process.env.REACT_APP_API_KEY,
-            "x-rapidapi-host": "the-legend-of-zelda.p.rapidapi.com"
+    const [data, setData] = useState([]);
+    const getRequest = async () => {
+        try {
+            const response = await fetch(
+                `https://the-legend-of-zelda.p.rapidapi.com/${queries[number]}`, 
+                {
+                    "method": "GET",
+                    "headers": {
+                        "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+                        "x-rapidapi-host": "the-legend-of-zelda.p.rapidapi.com"
+                    }
+                }
+            );
+            const json = await response.json();
+            setData(json.data);
+        } catch(error) {
+            console.log(error);
         }
-    })
-    .then(response => {
-        console.log(response.json());
-    })
-    .catch(err => {
-        console.error(err);
-    });
-
+    }
+    useEffect(() => {
+        getRequest();
+    }, [])
+    return data;
 }
