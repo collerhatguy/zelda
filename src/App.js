bf1da17ed408f2561fe7acc47d10c20dc9d69f4f
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-04-27 12:29:18 -0400

    finished header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, {useEffect} from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import useAPI from "./hooks/useAPI";
 import './App.css';
 
 function App() {
-  const data = useAPI(0);
-  useEffect(() => {
-    console.log(data);
-  }, [data])
+  
+  const queries = ["items", "places", "dungeons", "bosses", "monsters", "characters", "games"];
   return (
     <div className="App">
+      <header>
+        <nav>
+
+        </nav>
+      </header>
+      <Router>
+        <Switch>
+          {queries.map(query => {
+            return <Route path={`/${query}`} component={[`PageFor${query}`]} />
+          })}
+        </Switch>
+      </Router>
     </div>
   );
 }
